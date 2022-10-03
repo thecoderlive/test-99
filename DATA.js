e97e03ff65c0fc8e6107873dddba7e106d958b6e
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-04 03:00:45 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "65537d6d-bde2-4551-ae36-1f563eb743e9",
+    "home_feed": [
+        {
+            "id": "5e5c27b5-3b22-4ab2-a2fa-8cd29429a37d",
+            "artist_name": "second",
+            "home_carousel": [
+                {
+                    "id": "c233c421-6faf-4e6f-8c71-0e706205353f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/87.png"
+                },
+                {
+                    "id": "2c0f6be4-f72a-4ef1-9abc-9ab693884535",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/182.png"
+                },
+                {
+                    "id": "6e00e181-a82f-4092-928c-9d017b0f5460",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/194.png"
+                },
+                {
+                    "id": "13f32f8a-5e77-45e4-bff2-5adec3ec465b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/219.png"
+                },
+                {
+                    "id": "b4e50a61-1524-4b32-a4e3-2fc671d5c65e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/307.png"
+                }
+            ],
+            "art_caption": "however",
+            "art_description": "City six fact family nice ever. Authority minute while piece rich. Prove card necessary quickly source Mrs. Such stage deep fly gas show kitchen nature."
+        },
+        {
+            "id": "8e83412c-5fcc-4576-85d1-be2ca933a20b",
+            "artist_name": "dog",
+            "home_carousel": [
+                {
+                    "id": "cbac2df2-1c5d-40d1-9d22-f67fbe993284",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/62.png"
+                },
+                {
+                    "id": "37eda4b1-d076-4e2c-8868-fa6d0f2567b8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/359.png"
+                },
+                {
+                    "id": "d9f56974-d27a-4ec2-bf51-c98ba99f6d33",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/164.png"
+                },
+                {
+                    "id": "ac2c2df6-906a-4363-b7c9-6ac95111731c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/197.png"
+                },
+                {
+                    "id": "828346bc-c188-434a-b609-2bf7af91e291",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/216.png"
+                }
+            ],
+            "art_caption": "pretty",
+            "art_description": "Yard yourself fill other rate room. Probably suddenly news guy full. Employee movement such customer attention."
+        },
+        {
+            "id": "757d733a-8fb4-440f-bba8-3a3fa178ec6d",
+            "artist_name": "much",
+            "home_carousel": [
+                {
+                    "id": "0915dffb-9388-4219-bfe3-52fafc48ebf8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/352.png"
+                },
+                {
+                    "id": "0b473381-0d18-4c5f-bc60-bb21a4d9bd42",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/47.png"
+                },
+                {
+                    "id": "5d7515c6-668d-4868-8142-63eb69f17e22",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/21.png"
+                },
+                {
+                    "id": "12f559f5-8f3e-4b25-bb22-18be51076d7d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/197.png"
+                },
+                {
+                    "id": "402db620-ab30-4b21-89c0-b1d8b0acd514",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/44.png"
+                }
+            ],
+            "art_caption": "want",
+            "art_description": "Performance more near firm. Visit operation and huge black. High enter player take system rest."
+        },
+        {
+            "id": "47dab831-d976-4231-8f4e-d3f837546012",
+            "artist_name": "guy",
+            "home_carousel": [
+                {
+                    "id": "18a829c8-97df-496c-842d-5db80a9dd519",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/363.png"
+                },
+                {
+                    "id": "5f8b8270-3b44-4673-a0c4-0ccad2ed367c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/14.png"
+                },
+                {
+                    "id": "0642e7ac-083a-43b5-a033-05e530716964",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/72.png"
+                },
+                {
+                    "id": "79ac25ee-6c7b-49a5-88d1-db9e0fd7ad6b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/206.png"
+                },
+                {
+                    "id": "276de1d8-893c-4a28-9c15-acd7ebc76944",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/180.png"
+                }
+            ],
+            "art_caption": "doctor",
+            "art_description": "Maybe increase party race activity interesting. Real dark hotel remember hour common. Network child unit for raise environmental too our."
+        },
+        {
+            "id": "1e43f27c-568d-415c-be78-8087ea631107",
+            "artist_name": "fall",
+            "home_carousel": [
+                {
+                    "id": "d463e84f-452c-4843-845e-6af901c70ce9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/355.png"
+                },
+                {
+                    "id": "fd3fe125-768d-4c2f-a1d6-fbbe93a39ef5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/107.png"
+                },
+                {
+                    "id": "41140ce5-5060-4272-b363-1ff698f6af77",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/269.png"
+                },
+                {
+                    "id": "a43094dc-978e-4462-9329-a368beacc647",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/127.png"
+                },
+                {
+                    "id": "11d5e091-7de3-4586-a83b-e1ebc2578604",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/352.png"
+                }
+            ],
+            "art_caption": "be",
+            "art_description": "Yard everybody traditional age thousand so. Care role institution control. Herself interesting democratic treatment suddenly."
+        }
+    ]
+}
